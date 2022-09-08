0e11d10ef419438d4afdd6adccfa3500b4791340
Lucas Martins Cardozo <cardozofixe@gmail.com>
2022-09-07 21:44:26 -0300

    Exercicio Pensionato para fixação

---
+++ Vetores Exercicio Pensionato/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Vetores_Exercicio_Pensionato {
+    class Program {
+        static void Main(string[] args) {
+
+            Quarto[] estudante = new Quarto[10];
+
+            Console.Write("Quantos quartos serão alugados: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 0; i < n; i++) {
+                Console.WriteLine("Aluguel #" + i + ":");
+                Console.Write("Insira qual quarto  deseja reservar (0 - 9): ");
+                int quarto = int.Parse(Console.ReadLine());
+                Console.Write("Insina o nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Insina o email: ");
+                string email = Console.ReadLine();
+                estudante[quarto] = new Quarto(nome, email);
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Quartos Alugados: ");
+            for (int i = 0; i < 10; i++) {
+                if (estudante[i] != null) {
+                    Console.WriteLine(i + ": " + estudante[i]);
+
+
+                }
+            }
+
+        }
+    }
+}
---
+++ Vetores Exercicio Pensionato/Quarto.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Vetores_Exercicio_Pensionato {
+    class Quarto {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Quarto(string nome, string email) {
+            Nome = nome;
+            Email = email;
+        }
+        public override string ToString() {
+            return "Nome: " + Nome + " Email: " + Email;
+        }
+    }
+}
